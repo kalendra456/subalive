c38de2e3dd56b03335ce84a3e266afe8fd8ca841
yaswanth kalendra kukkala <80523148+kalendra456@users.noreply.github.com>
2024-04-04 10:09:54 +0530

    Update subalive.py
    updated -- to fix issuses

--- subalive.py
+++ subalive.py
@@ -5,10 +5,11 @@
 import requests
 import sys
 import argparse
+from concurrent.futures import ThreadPoolExecutor
 
 timeout = 3
 verbose = True
-version = "1.0.2"
+version = "1.0.5"
 banner = """
 ---------------------------------
 | s | u | b | a | l | i | v | e |
@@ -19,16 +20,27 @@ banner = """
       Updated by kalendra456
 """.format(version, sys.argv[0])
 
-usage = "\n [SYNTAX]  python {} target.txt -o output.txt".format(sys.argv[0])
+usage = "\n [SYNTAX]  python {} target.txt -o output.txt [-rm]".format(sys.argv[0])
 
 def create_file(user_file):
     with open(user_file, "w") as output_file:
         output_file.close()
 
+def check_subdomain(url, remove_redirects):
+    try:
+        req = requests.get(url, timeout=timeout, allow_redirects=not remove_redirects)
+        extra = "- HTTP {}".format(req.status_code) if verbose else ""
+        print("[+] Domain is online! ({}) {}".format(url, extra))
+        return url if not remove_redirects or req.status_code < 300 or req.status_code >= 400 else None
+    except requests.exceptions.RequestException:
+        print("[-] Domain is offline! ({})".format(url))
+        return None
+
 def main():
     parser = argparse.ArgumentParser(description="Check availability of subdomains and save all subdomains to a file.")
     parser.add_argument("input_file", help="Input file containing subdomains")
     parser.add_argument("-o", "--output", help="Output file to save all subdomains", default="alive.txt")
+    parser.add_argument("-rm", "--remove_redirects", action="store_true", help="Remove URLs that result in redirections (HTTP 3xx)")
     args = parser.parse_args()
 
     print(banner)
@@ -36,26 +48,15 @@ def main():
     with open(args.input_file, "r") as input_file:
         create_file(args.output)
 
-        for line in input_file:
-            url = "http://{}".format(line.strip())
-            try:
-                req = requests.get(url, timeout=timeout)
-                extra = "- HTTP {}".format(req.status_code) if verbose else ""
-                print("[+] Domain is online! ({}) {}".format(url, extra))
-                with open(args.output, "a") as output_file:
-                    output_file.write("{}\n".format(url))
-            except KeyboardInterrupt:
-                print("\n[-] Saving progress and exiting...")
-                with open(args.output, "a") as output_file:
-                    output_file.write("{}\n".format(url))
-                break
-            except requests.exceptions.Timeout:
-                print("[-] Domain timed out! ({})".format(url))
-            except requests.exceptions.ConnectionError:
-                print("[-] Domain may not exist! ({})".format(url))
-            except requests.exceptions.TooManyRedirects:
-                print("[-] Domain has too many redirects! ({})".format(url))
+        urls = ["http://{}".format(line.strip()) for line in input_file if line.strip()]
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            results = executor.map(lambda url: check_subdomain(url, args.remove_redirects), urls)
+
+        with open(args.output, "a") as output_file:
+            for result in results:
+                if result:
+                    output_file.write("{}\n".format(result))
 
 if __name__ == "__main__":
     main()
-
